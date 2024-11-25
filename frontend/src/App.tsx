675ac4f3bfdf62ecd71ac75a660ac4bfb98606ab
gamithf <gamithfernando311@gmail.com>
2024-11-25 10:16:01 +0530

    updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,6 +12,9 @@ import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
 import MainLayout from "./layout/MainLayout";
 import ChatPage from "./pages/chat/ChatPage";
 import AlbumPage from "./pages/album/AlbumPage";
+import AdminPage from "./pages/admin/AdminPage";
+import { Toaster } from "react-hot-toast";
+import NotFoundPage from "./pages/404/NotFoundPage";
 
 function App() {
   return (
@@ -26,13 +29,16 @@ function App() {
           }
         />
         <Route path="/auth-callback" element={<AuthCallbackPage />} />
+        <Route path="/admin" element={<AdminPage />} />
 
         <Route element={<MainLayout />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/chat" element={<ChatPage />} />
           <Route path="/albums/:albumId" element={<AlbumPage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
+      <Toaster/>
     </>
   );
 }
--- frontend/src/pages/admin/AdminPage.tsx
+++ frontend/src/pages/admin/AdminPage.tsx
@@ -1,26 +1,32 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import Header from './components/Header'
 import DashboardStats from './components/DashboardStats'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '../../components/ui/tabs'
 import SongsTabContent from './components/SongsTabContent'
 import AlbumsTabContent from './components/AlbumsTabContent'
+import { useAuthStore } from '@/stores/useAuthStore'
+import { useMusicStore } from '@/stores/useMusicStore'
+import { Music, Album } from 'lucide-react'
 
 const AdminPage = () => {
-    // const {isAdmin, isLoading} = useAuthStore();
+    const {isAdmin, isLoading} = useAuthStore();
 
-    // const {fetchAlbums, fetchSongs, fetchStats} = useMusicStore();
-    // useEffect(() => {
-    //     fetchAlbums();
-    //     fetchSongs();
-    //     fetchStats();
-    // }, [fetchAlbums, fetchSongs, fetchStats])
+    const {fetchAlbums, fetchSongs, fetchStats} = useMusicStore();   
     
-    // if (!isAdmin && !isLoading) return <div>Unauthorized</div>
+    useEffect(() => {
+        fetchAlbums();
+        fetchSongs();
+        fetchStats();
+    }, [fetchAlbums, fetchSongs, fetchStats])
+    
+    if (!isAdmin && !isLoading) return <div>Unauthorized</div>
 
     return (
         <div className='min-h-screen bg-gradient-to-b from-zinc-900 via-zinc-900 to-black text-zinc-100 p-8'>
             <Header/>
+
             <DashboardStats/>
+
             <Tabs defaultValue='songs' className='space-y-6'>
                 <TabsList className='p-1 bg-zinc-800/50'>
                     <TabsTrigger value="songs" className='data-[state=active]:bg-zinc-700'>
